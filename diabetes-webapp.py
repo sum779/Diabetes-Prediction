5dc96f0996ff7a36f790d1b81658b252b71b0729
Sumit Aryal <164495250+sum779@users.noreply.github.com>
2024-06-08 20:54:58 +1000

    Update diabetes-webapp.py

--- diabetes-webapp.py
+++ diabetes-webapp.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import sklearn
 from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split, GridSearchCV
